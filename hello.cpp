79a3e8114e5cf8d0b9503d90fb5e3441786dc1dd
Lizhijian-123 <222988145@qq.com>
2023-05-24 16:22:56 +0800

    添加了早上好的功能,第四次提交

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,7 @@
 int main() {
 	cout << "git learning hello world" << endl;
 	cout << "hello diercitijiao " << endl;
-	cout << "3 i don change erroe " << endl;
+	cout << "3 i don change error " << endl;
+	cout << " Good Morning" << endl;
 	return 0;
 }
